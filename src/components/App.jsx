081ab377694723431e84bce64d0ef5bed3217e0c
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2023-03-29 21:41:29 +0200

    header made

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import { Searchbar } from './Searchbar/Searchbar';
+const API_KEY = '33245282-6e238748bd483492097eba1b8';
+export class App extends Component {
+  state = {
+    images: [],
+  };
+  onSubmit = inputValue => {
+    console.log(inputValue);
+  };
+  render() {
+    return (
+      <>
+        <Searchbar onSubmit={this.onSubmit}></Searchbar>
+      </>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,43 @@
+import { Component } from 'react';
+import css from './Searchbar.module.css';
+
+export class Searchbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+    };
+  }
+  handleChange = evt => {
+    this.setState({ inputValue: evt.currentTarget.value });
+    console.log(this.state.inputValue);
+  };
+  handleSubmit = evt => {
+    const { inputValue } = this.state;
+    const { onSubmit } = this.props;
+    evt.preventDefault();
+    onSubmit(inputValue);
+  };
+  render() {
+    const { inputValue } = this.state;
+    return (
+      <header className={css.bar}>
+        <form className={css.form} onSubmit={this.handleSubmit}>
+          <button type="submit" className={css.button}>
+            <span className={css.buttonLabel}>Search</span>
+          </button>
+
+          <input
+            value={inputValue}
+            onChange={this.handleChange}
+            className={css.input}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
